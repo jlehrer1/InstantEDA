c3e149675d2ff0c6eccf21cfa9d92ea59c1c9df1
Julian Lehrer <jmlehrer@ucsc.edu>
2020-05-16 22:57:04 -0700

    added data impute function

--- cleaning/clean.py
+++ cleaning/clean.py
@@ -4,5 +4,9 @@ from sklearn.impute import KNNImputer
 
 def impute_data(df: pd.DataFrame) -> pd.DataFrame:
     imputer = KNNImputer()
+    for column in df.columns:
+        if df[column].isna().sum() != 0:
+            df[column] = imputer.fit_transform(df[[column]])
+    return df
     
  
