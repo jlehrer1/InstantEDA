7004b06013d58d9634b6f19a6a4f0a0898ea613a
Julian Lehrer <jmlehrer@ucsc.edu>
2020-05-16 22:42:05 -0700

    started structure

--- run.py
+++ run.py
@@ -1,15 +1,23 @@
 import os
 import argparse
+import pandas pd
+
+from cleaning import clean
 
 parser = argparse.ArgumentParser()
-parser.add_argument("input", help="Data file in .csv form") #add other file types later
+parser.add_argument("input", help="Path to .csv or .xlsx data file") #add other file types later
 parser.add_argument("-w", "--write", help="Write pdf to specified directory if it exists, else writes to current")
 
 if __name__ == "__main__":
     args = vars(parser.parse_args())
+
+    if not args["input"].endswith('.csv') or not args["input"].endswith('.xlsx'):
+        raise TypeError("Error: input is not a .csv file")
     if args["write"] is not None and not os.path.isdir(args["write"]):
         raise NotADirectoryError("Directory does not exist or not a valid file path")
-    if not args["input"].endswith('.csv'):
-        raise TypeError("Error: input is not a .csv file")
-
     
+    df = pd.read_csv(args["input"])
+
+    # impute the data
+    df = clean.impute_data(df)
+
