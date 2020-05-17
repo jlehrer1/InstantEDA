c19fc8004f7142153a8fff8b5168bd82fc246db6
Julian Lehrer <jmlehrer@ucsc.edu>
2020-05-16 22:09:27 -0700

    added argument parsing

--- run.py
+++ run.py
@@ -1,4 +1,15 @@
 import os
+import argparse
+
+parser = argparse.ArgumentParser()
+parser.add_argument("input", help="Data file in .csv form") #add other file types later
+parser.add_argument("-w", "--write", help="Write pdf to specified directory if it exists, else writes to current")
 
 if __name__ == "__main__":
-    pass
+    args = vars(parser.parse_args())
+    if args["write"] is not None and not os.path.isdir(args["write"]):
+        raise NotADirectoryError("Directory does not exist or not a valid file path")
+    if not args["input"].endswith('.csv'):
+        raise TypeError("Error: input is not a .csv file")
+
+    
