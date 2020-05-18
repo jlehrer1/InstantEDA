55e6a74309e236c90e20dac7fccfdb25f9a63dab
Julian Lehrer <jmlehrer@ucsc.edu>
2020-05-18 14:15:34 -0700

    fixed num nans, added percent nan generator

--- vizualization/visualization.py
+++ vizualization/visualization.py
@@ -4,31 +4,44 @@ import plotly.graph_objects as go
 
 # RAW DATA METHODS
 def num_nan_plot(df: pd.DataFrame):
-    num_nans = sum([True for idx, row in df.iterrows() if any(row.isnull())])
     fig = go.Figure(
         data=go.Bar(x=df.columns, y=(df.isnull().sum())),
         layout=go.Layout(
             title={
-                'text':'Number of NaN values in each column',
+                'text':'Number of NaN Values in Each Column where DataFrame Shape is {}'.format(df.shape),
                 'y':0.9,
                 'x':0.5,
                 'xanchor':'center',
                 'yanchor':'top',
-            }
+            },
+            xaxis_title='Column',
+            yaxis_title='Number of NaN',
         )
     )
+    return fig
 
 def percent_nan_plot(df: pd.DataFrame):
-    pass
+    fig = go.Figure(
+        data=go.Bar(x=df.columns, y=(df.isnull().sum() / df.shape[0]) * 100),
+        layout=go.Layout(
+            title={
+                'text':'Percent of NaN values in Each Column',
+                'y':0.9,
+                'x':0.5,
+                'xanchor':'center',
+                'yanchor':'top',
+            },
+            xaxis_title='Column',
+            yaxis_title='Percent of NaN',
+        )
+    )
+    return fig
 
 # CLEAN DATA METHODS
 def generate_correlation_plot(df: pd.DataFrame):
-    fig = go.Figure(data=go.Heatmap(
-        z=df.corr(),
-        x=df.corr().columns,
-        y=df.corr().columns,
-        hoverongaps=False,
-    ), layout=go.Layout(
+    fig = go.Figure(
+    data=go.Heatmap(z=df.corr(), x=df.corr().columns, y=df.corr().columns, hoverongaps=False), 
+    layout=go.Layout(
         height=900,
         width=900,
         title={
