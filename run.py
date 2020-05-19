58b94880225939fe20ddafef87ab20c7a6e6e7ab
Julian Lehrer <jmlehrer@ucsc.edu>
2020-05-18 22:34:58 -0700

    added image exporting

--- run.py
+++ run.py
@@ -14,23 +14,25 @@ parser.add_argument(
 if __name__ == "__main__":
     args = vars(parser.parse_args())
 
+    dir_w = args["write"]
     if not args["input"].endswith('.csv') or not args["input"].endswith('.xlsx'):
         raise TypeError("Error: input is not a .csv or .xlsx file")
-    if args["write"] is not None and not os.path.isdir(args["write"]):
+    if dir_w is not None and not os.path.isdir(dir_w):
         raise NotADirectoryError(
             "Directory does not exist or not a valid file path")
+    else:
+        try:
+            os.makedirs(dir_w)
+        except FileExistsError:
+            print("Write location exists, continuing...")
 
     df = pd.read_csv(args["input"])
-
     num_nan_plot = visualization.num_nan_plot(df)
     percent_nan_plot = visualization.percent_nan_plot(df)
 
-    df_clean = clean.impute_data(df) if key.lower() == 'y' else quit()
-
+    df_clean = clean.impute_data(df)
     corr_plot = visualization.corr_plot(df)
 
-    if args["write"] is not None:
-        try:
-            os.makedirs(args["write"])
-        except FileExistsError:
-            pass
+    num_nan_plot.write_image(os.path.join(dir_w, 'num_nan.png'))
+    percent_nan_plot.write_image(os.path.join(dir_w, 'percent_nan.png'))
+    corr_plot.write_image(os.path.join(dir_w, 'correlations.png'))
