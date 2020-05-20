0fa0d9287af1fe84a446c8b42280ec7ecb8f5d9b
Julian Lehrer <jmlehrer@ucsc.edu>
2020-05-19 18:39:56 -0700

    initial class

--- quickplotter.py
+++ quickplotter.py
@@ -4,8 +4,20 @@ from visualization import visualization
 from cleaning import clean
 
 class QuickPlotter:
-    def __init__(self, df, y=None):
+    def __init__(self, df: pd.DataFrame, y: str = None, subset: list = None, diff : list = None):
         self.y = y
         self.df = df 
+        self.subset = subset
+        self.diff = diff 
+
+    def plot(self):
+        f1 = visualization.num_nan_plot(self.df).show()
+        f2 = visualization.percent_nan_plot(self.df).show()
+        f3 = visualization.generate_correlation_plot(self.df).show()
+
+        plots = {'num_nan': f1, 'percent_nan': f2, 'corr': f3}
+
+
+
 
 
