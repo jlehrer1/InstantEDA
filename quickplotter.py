70f060e434fd1410faa491f6465dbfe0c14e71ec
Julian Lehrer <jmlehrer@ucsc.edu>
2020-05-20 20:49:30 -0700

    more methods

--- quickplotter.py
+++ quickplotter.py
@@ -4,26 +4,36 @@ from visualization import visualization
 from cleaning import clean
 
 class QuickPlotter:
-    def __init__(self, df: pd.DataFrame, subset: list = None, diff: list = None):
-        if subset is not None and diff is not None:
-            print("add error here")
-
+    def __init__(self, df: pd.DataFrame):
         self.df = df
-        self.clean_df = clean.clean(df) #add ability to change params later
-        self.subset = subset
-        self.diff = diff
+        self.df_clean = clean.clean(df) #add ability to change params later
+        self.plotlist = ['num_nan', 'percent_nan', 'correlation']
 
-    def plot(self):
-        if self.subset is not None:
-            pass 
-        if self.diff is not None:
-            pass
-        f1 = visualization.num_nan_plot(self.df).show()
-        f2 = visualization.percent_nan_plot(self.df).show()
-        f3 = visualization.generate_correlation_plot(self.df).show()
+    def _get_plotlist(self):
+        df1 = visualization.num_nan_plot(self.df)
+        df2 = visualization.percent_nan_plot(self.df)
+        df3 = visualization.generate_correlation_plot(self.df_clean)
 
-        plots = {
-            'num_nan': f1, 
-            'percent_nan': f2, 
-            'corr': f3
+        return {
+            'num_nan': df1,
+            'percent_nan': df2,
+            'correlation': df3,
         }
+
+    def plot_all(self):
+        plots = self._get_plotlist()
+        for plot in plots:
+            plots[plot].show()
+    
+    def plot(self, subset: list = None, diff: list = None):
+        to_plot = []
+        if subset is None and diff is None:
+            # plot is called without subset/diff specified, just plot all
+            self.plot_all()
+        elif subset is not None and set(subset).issubset(set(self.plotlist)):
+            pass
+        elif diff is not None and set(diff).issubset(set(self.plotlist)):
+            pass
+        else:
+            # 
+            raise ValueError("subset or diff contains improper values")
