79d82f99ec59d11213a7e988a7c186d4e1da2708
Julian Lehrer <jmlehrer@ucsc.edu>
2020-06-18 12:34:03 -0700

    formatted with pep8

--- quickplotter.py
+++ quickplotter.py
@@ -1,12 +1,13 @@
-import pandas as pd 
+import pandas as pd
 
 from visualization import visualization
 from cleaning import clean
 
+
 class QuickPlotter:
     def __init__(self, df: pd.DataFrame):
         self.df = df
-        self.df_clean = clean.clean(df) #add ability to change params later
+        self.df_clean = clean.clean(df)  # add ability to change params later
         self.plotlist = {
             'common': ['num_nan', 'percent_nan', 'correlation'],
             'pairwise': ['pairwise'],
@@ -42,8 +43,9 @@ class QuickPlotter:
         elif diff is not None and set(diff).issubset(set(self.plotlist)):
             self._plot(list(set(self.plotlist) - set(diff)))
         else:
-            raise ValueError("subset or diff contains improper values. Check plotlist attribute for appropriate values")
-    
+            raise ValueError(
+                "subset or diff contains improper values. Check plotlist attribute for appropriate values")
+
     def pairwise(self, feature_subset: list = None, feature_diff: list = None):
         """Plots each feature X_i against X_j
             Parameters
@@ -52,13 +54,16 @@ class QuickPlotter:
             diff: plot all features except those in diff
         """
         if feature_subset is None and feature_diff is None:
-            visualization.pairwise_plot(self.df_clean, self.df_clean.columns).show()
+            visualization.pairwise_plot(
+                self.df_clean, self.df_clean.columns).show()
         elif feature_subset is not None and set(feature_subset).issubset(set(self.df.columns)):
             visualization.pairwise_plot(self.df_clean, feature_subset).show()
         elif feature_diff is not None and set(feature_diff).issubset(set(self.df.columns)):
-            visualization.pairwise_plot(self.df_clean, list(set(self.df_clean.columns) - set(feature_diff))).show()
+            visualization.pairwise_plot(self.df_clean, list(
+                set(self.df_clean.columns) - set(feature_diff))).show()
         else:
-            raise ValueError("feature_subset or feature_diff contain values that are not column names")        
+            raise ValueError(
+                "feature_subset or feature_diff contain values that are not column names")
 
     def variance(self, subset: list = None, diff: list = None):
         """Plots pairwise variance"""
--- visualization/visualization.py
+++ visualization/visualization.py
@@ -5,34 +5,37 @@ import plotly.express as px
 import warnings
 
 # RAW DATA METHODS
+
+
 def num_nan_plot(df: pd.DataFrame):
     fig = go.Figure(
         data=go.Bar(x=df.columns, y=(df.isnull().sum())),
         layout=go.Layout(
             title={
-                'text':'Number of NaN Values in Each Column',
-                'y':0.9,
-                'x':0.5,
-                'xanchor':'center',
-                'yanchor':'top',
+                'text': 'Number of NaN Values in Each Column',
+                'y': 0.9,
+                'x': 0.5,
+                'xanchor': 'center',
+                'yanchor': 'top',
             },
             xaxis_title='Column',
             yaxis_title='Number of NaN values',
-            yaxis = dict(rangemode='nonnegative'),
+            yaxis=dict(rangemode='nonnegative'),
         )
     )
     return fig
+
 
 def percent_nan_plot(df: pd.DataFrame):
     fig = go.Figure(
         data=go.Bar(x=df.columns, y=(df.isnull().sum() / df.shape[0])),
         layout=go.Layout(
             title={
-                'text':'Percent of NaN values in Each Column',
-                'y':0.9,
-                'x':0.5,
-                'xanchor':'center',
-                'yanchor':'top',
+                'text': 'Percent of NaN values in Each Column',
+                'y': 0.9,
+                'x': 0.5,
+                'xanchor': 'center',
+                'yanchor': 'top',
             },
             xaxis_title='Column',
             yaxis_title='Percent NaN',
@@ -40,25 +43,30 @@ def percent_nan_plot(df: pd.DataFrame):
         )
     )
     return fig
-    
+
 # CLEAN DATA METHODS
+
+
 def correlation_plot(df: pd.DataFrame):
     """Returns correlation plot for all features in given DataFrame"""
     if df.isna().sum().sum() > 0:
-        warnings.warn("DataFrame contains NaN values, correlations are not well defined")
+        warnings.warn(
+            "DataFrame contains NaN values, correlations are not well defined")
     fig = go.Figure(
-    data=go.Heatmap(z=df.corr(), x=df.corr().columns, y=df.corr().columns, hoverongaps=False), 
+        data=go.Heatmap(z=df.corr(), x=df.corr().columns,
+                        y=df.corr().columns, hoverongaps=False),
         layout=go.Layout(
-        height=900,
-        width=900,
-        title={
-            'text': 'Variable Correlation Matrix',
-            'x': 0.5,
-            'xanchor': 'center',
-            'yanchor': 'top'
-        }
-    ))
+            height=900,
+            width=900,
+            title={
+                'text': 'Variable Correlation Matrix',
+                'x': 0.5,
+                'xanchor': 'center',
+                'yanchor': 'top'
+            }
+        ))
     return fig
+
 
 def pairwise_plot(df: pd.DataFrame, cols: list):
     """Returns pairplot for features listed in cols parameter"""
@@ -66,9 +74,8 @@ def pairwise_plot(df: pd.DataFrame, cols: list):
         data=go.Splom(
             dimensions=[{'label': i, 'values': df[i]} for i in cols],
         ),
-        layout=go.Layout(            
+        layout=go.Layout(
             title='Pairwise Matrix Plot',
         )
     )
     return fig
-
