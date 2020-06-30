0a6f1d01c2eb8a46c1a439d696cbf0e197246fc3
Julian Lehrer <jmlehrer@ucsc.edu>
2020-06-30 09:22:01 -0700

    Files created after python setup.py sdist

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ setup(
   author = 'Julian Lehrer',                   # Type in your name
   author_email = 'julianmlehrer@gmail.com',      # Type in your E-Mail
   url = 'https://github.com/jlehrer1/InstantEDA',   # Provide either the link to your github or to your website
-  download_url = 'https://github.com/jlehrer1/InstantEDA/archive/0.1.tar.gz'    # I explain this later on
+  download_url = 'https://github.com/jlehrer1/InstantEDA/archive/0.1.tar.gz',    # I explain this later on
   keywords = ['VISUALIZATION', 'PYTHON', 'DATA SCIENCE'],   # Keywords that define your package best
   install_requires=[            # I get to this in a second
     	'numpy',
