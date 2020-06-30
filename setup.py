9c8d2e3fb26d1dc0bfc60496dc35fae4d1f03c47
Julian Lehrer <jmlehrer@ucsc.edu>
2020-06-30 09:34:13 -0700

    Published to PyPi, published files live under dist/

--- setup.py
+++ setup.py
@@ -5,6 +5,27 @@ setup(
   version = '0.1',      # Start with a small number and increase it with every change you make
   license='MIT',        # Chose a license from here: https://help.github.com/articles/licensing-a-repository
   description = 'Instantly generate common EDA plots without cleaning your DataFrame',   # Give a short description about your library
+  long_description="""# Instant EDA (Work in progress!)
+	Instantly generate common exploratory data plots without having to worry about cleaning your data.
+
+	To setup the proper development environment, run `conda env create -f environment.yml`
+
+	Usage:
+	```python3
+	plotter = quickplotter.QuickPlotter(df: pd.DataFrame) #creates a QuickPlotter object with the given DataFrame
+	
+	plotter.common(subset=['correlation', 'percent_nan']) #plots correlation between features, and percent nan in each column
+	
+	plotter.distribution(column_subset=df.columns[0:4]) #plots distributions for the first four columns in the DataFrame
+
+	```
+
+	The quickplot module works mainly with two specifications, `subset` and `diff`. 
+
+	For any `subset`-like list, the items in the list will be used. For any `diff`-like list, all items *except* those in the list will be used. 
+
+	To specifiy column `subset`'s or `diff`'s, call each plot individually or call `.common` with the `column_subset` or `column_diff` attributes (need to be added as of 6/18/20).
+	""",
   author = 'Julian Lehrer',                   # Type in your name
   author_email = 'julianmlehrer@gmail.com',      # Type in your E-Mail
   url = 'https://github.com/jlehrer1/InstantEDA',   # Provide either the link to your github or to your website
